875d964a28592b37100735e3f70922f6b5a3659b
arukondakaveri18-stack <arukondakaveri18@gmail.com>
2026-04-05 19:46:55 +0530

    Add files via upload

---
+++ password_generator .py
@@ -0,0 +1,20 @@
+import string
+
+print("===================================")
+print("     🔐 PASSWORD GENERATOR         ")
+print("===================================\n")
+
+# Predefined password (given by you)
+password = "Arukondakaveri@"
+
+print("⏳ Checking password generation...\n")
+
+# Display password
+print("===================================")
+print("✅ Password generated successfully!")
+print("🔑 Generated Password:", password)
+print("===================================\n")
+
+print("✔ Password length:", len(password))
+print("✔ Secure password includes letters and special characters")
+print("✔ Keep it safe and do not share it")
